3a023b27f60e0d862171c44405a39da40cf65db3
Maikel Eckelboom <maikeleckelboom@outlook.com>
2023-02-28 23:24:38 +0100

    test: auto-deploy test

--- packages/ui/components/Button.vue
+++ packages/ui/components/Button.vue
@@ -8,15 +8,46 @@ defineEmits<{
 </script>
 
 <template>
-  <button class="btn" :disabled="disabled" @click="$emit('submit')">
+  <button :disabled="disabled" class="btn" @click="$emit('submit')">
     <slot>Button</slot>
   </button>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .btn {
-  padding: 1rem;
-  background-color: gainsboro;
-  color: black;
+  padding: 12px 24px;
+  background-color: hsla(0, 80%, 50%, 1);
+  color: white;
+  border-radius: 24px;
+  overflow: hidden;
+  position: relative;
+  border: none;
+  transition: background-color cubic-bezier(0.4, 0, 0.2, 1) 0.2s;
+
+  &:hover {
+    background-color: hsl(0, 80%, 40%);
+    cursor: pointer;
+  }
+
+  &:active {
+    background-color: hsl(0, 80%, 30%);
+    cursor: pointer;
+    box-shadow: 0 0 0 8px hsla(0, 80%, 50%, 0.2);
+  }
+
+  &:focus-visible {
+    outline: none;
+    box-shadow: 0 0 0 2px hsla(0, 80%, 50%, 0.5);
+  }
+
+  &:disabled {
+    background-color: hsl(0, 0%, 80%);
+    color: hsl(0, 0%, 50%);
+    cursor: not-allowed;
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 2px hsla(0, 80%, 50%, 0.2);
+  }
 }
 </style>
