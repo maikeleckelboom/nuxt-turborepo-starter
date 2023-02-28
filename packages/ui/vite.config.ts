911403a53adbee68c5332fc586c530a9231a53c3
Maikel Eckelboom <maikeleckelboom@outlook.com>
2023-02-28 02:52:59 +0100

    initial commit

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,12 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    reporters: ['verbose'],
+    include: ['tests/**/*.test.ts'],
+    globals: true,
+    environment: 'jsdom',
+  },
+})
