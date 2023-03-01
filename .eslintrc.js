a25030b20218169b106d0d82f45ba5279d2de1d9
Maikel Eckelboom <maikeleckelboom@outlook.com>
2023-03-01 01:42:18 +0100

    refactor: changed eslint-custom-config to config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ['@mannesmaikel/eslint-config-custom'],
+  // This tells ESLint to load the config from the package `config`
+  extends: ['@mannesmaikel/config'],
   settings: {
     next: {
       rootDir: ['apps/*/'],
